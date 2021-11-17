a114c2d464a5afc45c15966dc7a8f5cabb9e0cf2
msmith7458 <89811344+msmith7458@users.noreply.github.com>
2021-11-17 17:44:56 -0500

    Update Bubble Wrap Generator.cpp

--- C++/Bubble Wrap Generator.cpp
+++ C++/Bubble Wrap Generator.cpp
@@ -4,25 +4,31 @@
 
 using namespace std;
 
+void printWrap(int rows, int cols, string word);
+
 int main()
 {
 	int rows, cols = 0;
 	string word;
 
 	cout << "How many columns?" << endl;
-	cin >> rows;
-	cout << "How many rows?" << endl;
 	cin >> cols;
+	cout << "How many rows?" << endl;
+	cin >> rows;
 	cout << "What should each cell say?" << endl;
 	cin >> word;
 
-	for (int i = 0; i < rows; i++)
+    printWrap(rows, cols, word);
+}
+
+void printWrap(int rows, int cols, string word)
+{
+    for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < cols; j++)
 		{
 			cout << "||*" + word + "*|| ";
 		}
-
 		cout << endl;
 	}
 }
